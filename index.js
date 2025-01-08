e48cb88a4f445db4aafe32116945cbf9ed5e7283
Tim Rodney-Davis Nchamcham <timrodneydavis189@gmail.com>
2025-01-08 09:40:06 +0000

    created files

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1. Create two variables, firstCard and secondCard. 
+let firstCard = 7
+let secondCard = 10
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard
